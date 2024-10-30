065dfb17ed5d83fe36271cd2ce18e88d2e9f72d5
lodoes <lo@Lotfi.localdomain>
2024-10-30 22:34:19 +0100

    Correction de la navigation avec <Link> pour Next.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,21 +1,31 @@
 import React from 'react';
+import Link from 'next/link'; // Importez Link de next/link
 import styles from '../styles/Navbar.module.css';
 
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <div className={styles.logo}>
-        <a href="/">MyApp</a>
+        {/* Utilisez Link pour la navigation */}
+        <Link href="/">
+          <a>MyApp</a>
+        </Link>
       </div>
       <ul className={styles.navLinks}>
         <li>
-          <a href="/">Home</a>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
         </li>
         <li>
-          <a href="/about">About</a>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
         </li>
         <li>
-          <a href="/contact">Contact</a>
+          <Link href="/contact">
+            <a>Contact</a>
+          </Link>
         </li>
       </ul>
     </nav>
