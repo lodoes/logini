181d6fd396feb66192ee4d8da7b78b6034dfe77d
lodoes <lo@Lotfi.localdomain>
2024-11-15 13:40:00 +0100

    Replace Font Awesome with react-icons for map marker

--- components/LatestResidences.js
+++ components/LatestResidences.js
@@ -3,7 +3,6 @@
 import React, { useState, useEffect } from 'react';
 import { supabase } from '../lib/supabaseClient';
 import ResidenceCard from './ResidenceCard';
-import { FaMapMarkerAlt } from 'react-icons/fa';
 
 
 const LatestResidences = () => {
